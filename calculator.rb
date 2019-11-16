9588a01e78d67fc34d9d65745a50b5bfd2b7c8a8
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-16 23:25:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,4 @@ second_number = 2
 sum = first_number + second_number
 subtract = first_number - second_number
 multiply = first_number * second_number
-divide = first_number  second_number
+divide = first_number / second_number
